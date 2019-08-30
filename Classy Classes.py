0bfb044e90c079d3728ba10ebf862b3b21b2a636
Orest <47123784+Orikfw@users.noreply.github.com>
2019-08-30 17:19:35 +0300

    Update Classy Classes.py

--- Classy Classes.py
+++ Classy Classes.py
@@ -1,11 +1,3 @@
 class Person():
     def __init__(self, name, age):
         self.info="{}s age is {}".format(name, age)
-
-
-names=["john","matt","alex","cam"]
-ages=[16,25,57,39]
-for i in range(4):
-    name, age=names[i], ages[i]
-    person=Person(name, age)
-    print(person.info)
